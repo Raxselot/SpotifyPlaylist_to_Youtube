896043008649ceac120fd097b8c034b1626e5937
Raxselot <139113825+Raxselot@users.noreply.github.com>
2024-09-19 09:37:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
+import os
+import logging
 from SpotifyService import SpotifyService
 from YouTubeService import YouTubeService
 from PlaylistConverter import PlaylistConverter
+from dotenv import load_dotenv
 
 def main():
-    spotify_client_id = ""
-    spotify_client_secret = "" 
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
 
-    youtube_client_secrets_file = "client_secrets.json"
+    load_dotenv()
 
-    spotify_service = SpotifyService(spotify_client_id, spotify_client_secret)
-    youtube_service = YouTubeService(youtube_client_secrets_file)
-    
-    converter = PlaylistConverter(spotify_service, youtube_service)
-    
-    spotify_playlist_id = "37i9dQZF1DXcBWIGoYBM5M"     # https://open.spotify.com/playlist/              --->     5FghkTTuq4SNdFJAp5uHQa   <--- That part
-    
-    youtube_playlist_title = "Converted Spotify Playlist"
-    youtube_playlist_description = "This playlist was converted from Spotify to YouTube"
-    
-    converter.convert(spotify_playlist_id, youtube_playlist_title, youtube_playlist_description)
+    spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+    spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+    if not spotify_client_id or not spotify_client_secret:
+        logging.error("Spotify-Client-ID und/oder -Secret sind nicht gesetzt.")
+        return
+
+    youtube_client_secrets_file = os.getenv("YOUTUBE_CLIENT_SECRETS_FILE", "client_secrets.json")
+    if not os.path.exists(youtube_client_secrets_file):
+        logging.error(f"YouTube-Client-Geheimnisse-Datei '{youtube_client_secrets_file}' nicht gefunden.")
+        return
+
+    try:
+        spotify_service = SpotifyService(spotify_client_id, spotify_client_secret)
+        youtube_service = YouTubeService(youtube_client_secrets_file)
+    except Exception as e:
+        logging.error(f"Fehler bei der Initialisierung der Dienste: {e}")
+        return
+
+    converter = PlaylistConverter(spotify_service, youtube_service, delay_between_requests=1)
+
+    spotify_playlist_id = "5FghkTTuq4SNdFJAp5uHQa" 
+    youtube_playlist_title = "Push Youor Luck"
+    youtube_playlist_description = "Diese Playlist wurde von Spotify zu YouTube konvertiert."
+
+    youtube_playlist_id = converter.convert(spotify_playlist_id, youtube_playlist_title, youtube_playlist_description)
+    if youtube_playlist_id:
+        logging.info(f"YouTube-Playlist erfolgreich erstellt: {youtube_playlist_id}")
+    else:
+        logging.error("Fehler bei der Erstellung der YouTube-Playlist.")
 
 if __name__ == "__main__":
     main()
